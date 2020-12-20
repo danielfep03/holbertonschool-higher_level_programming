4c83d203e7f37a96930c773c54b0eb223fd05c0f
Daniel <danielfep.am@gmail.com>
2020-12-20 00:08:53 -0500

    Update code

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for key_iterator, value_iterator in a_dictionary.items():
-        if key_iterator == key:
-            if value == value_iterator:
-                return a_dictionary
-            else:
-                a_dictionary[key] = value
-
+    a_dictionary.setdefault(key, value)
     a_dictionary[key] = value
     return a_dictionary
