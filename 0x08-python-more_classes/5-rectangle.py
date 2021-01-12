7bf1337f43d3f9306f291b1e21a355223d714616
Daniel Amado <danielfep.am@gmail.com>
2021-01-11 23:25:45 -0500

    Rectangle with a del method

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+''' Define a Rectangle Class'''
+
+
+class Rectangle:
+    '''Rectangle with a del method'''
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    def __str__(self):
+        if self.height == 0 or self.width == 0:
+            return("")
+        else:
+            return (("#" * self.width + "\n") * self.height).strip()
+
+    def __repr__(self):
+        return("Rectangle({:d}, {:d})".format(self.width, self.height))
+
+    def __del__(self):
+        print("Bye rectangle...")
+
+    def area(self):
+        if self.height == 0 or self.width == 0:
+            return 0
+        return self.height * self.width
+
+    def perimeter(self):
+        if self.height == 0 or self.width == 0:
+            return 0
+        return (self.height * 2) + (self.width * 2)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
