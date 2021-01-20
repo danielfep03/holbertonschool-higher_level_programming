07f29abb843dae3d3d6ec4a787bea18c831a473e
Daniel Amado <danielfep.am@gmail.com>
2021-01-20 12:01:56 -0500

    Write into a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' Code to learn how to write into a file '''
+
+
+def write_file(filename="", text=""):
+    ''' Write into filename the text '''
+    with open(filename, "w") as f:
+        f.write
+    return len(text)
