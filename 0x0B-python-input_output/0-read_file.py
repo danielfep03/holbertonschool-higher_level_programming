27f1f1e733f672fbc9c272f8899f95a8190f2891
Daniel Amado <danielfep.am@gmail.com>
2021-01-20 16:51:37 -0500

    Added encpding UTF-8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-'''  Code to lear to read a file '''
+'''  Code to learn how to read a file '''
 
 
-def read_file(filename=""):
+def read_file(filename="", encoding='UTF-8'):
     ''' Read filename '''
     with open(filename) as f:
         for line in f:
