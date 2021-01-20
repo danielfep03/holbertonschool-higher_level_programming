b44d28c467ca2ad649776a207eb94d7fd8bcd17c
Daniel Amado <danielfep.am@gmail.com>
2021-01-19 20:27:38 -0500

    Pep8 Style

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 ''' Code to learn to use issubclass()'''
 
+
 def inherits_from(obj, a_class):
     ''' Check if the object is an instance of a class '''
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
-       return True
+        return True
     return False
