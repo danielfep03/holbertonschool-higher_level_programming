924116893ee74c961d14ef72156a78ff8758053c
Daniel Amado <danielfep.am@gmail.com>
2021-01-20 17:12:13 -0500

    Pep8 style

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     ''' Read filename '''
     with open(filename, encoding='UTF-8') as f:
         for i in f:
-            print (i, end='')
+            print(i, end='')
