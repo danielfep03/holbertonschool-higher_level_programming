01062c22f1022f3d956be802e52842061d93ae58
Daniel Amado <danielfep.am@gmail.com>
2021-01-19 21:24:45 -0500

    Pep8 style

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,6 +4,7 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     ''' New integer_validator method that validates value parameter '''
     def __init__(self, width, height):
