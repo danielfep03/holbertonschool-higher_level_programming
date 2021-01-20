6b48bceb67a135ad226169bc2af9b1a4d4379318
Daniel Amado <danielfep.am@gmail.com>
2021-01-19 21:23:49 -0500

    Validation of constructor fixed

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,7 +7,8 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     ''' New integer_validator method that validates value parameter '''
     def __init__(self, width, height):
-        self.integer_validator(width, height)
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
         self.__width = width
         self.__height = height
 
