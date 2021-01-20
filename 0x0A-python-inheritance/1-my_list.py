e71db28318b6dc2edcd8e1c238088d816a4a5e44
Daniel Amado <danielfep.am@gmail.com>
2021-01-19 19:20:27 -0500

    Class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' File to learn to use inheritance '''
+
+
+class MyList(list):
+    '''Class that inherits from list''' 
+
+    def print_sorted(self):
+        new_list = self.copy()
+        new_list.sort()
+        print(new_list)
+
