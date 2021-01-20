f134c1ccbf955eb4105664c2149488960beef494
Daniel Amado <danielfep.am@gmail.com>
2021-01-19 20:45:32 -0500

    Class with area method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' BaseGeometry class with only one method '''
+
+
+class BaseGeometry:
+    ''' Class with area method '''
+    def area(self):
+        raise Exception("area() is not implemented")
