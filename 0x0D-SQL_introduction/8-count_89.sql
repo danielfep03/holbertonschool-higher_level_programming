5597d4cdf9ed26c6156819531116e9f10053a1b5
Daniel Amado <danielfep.am@gmail.com>
2021-03-01 22:43:39 -0500

    Script that displays the number of records with id = 89 in a tabl

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- Script that displays the number of records with id = 89 in a table
-SELECT * FROM `first_table` WHERE (`id` = 89 AND `data` = 'brandA');
+SELECT * FROM `first_table` WHERE (`id` = 89);
