a1a71474df5a54f5805a4633c164df3358738047
Daniel Amado <danielfep.am@gmail.com>
2021-03-02 20:37:50 -0500

    Script that creates the database hbtn_0d_usa and the table cities

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Script that creates the database hbtn_0d_usa and the table cities
+-- Creates a new database and a new table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+	`id` INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	`state_id` INTEGER NOT NULL FOREIGN KEY REFERENCE state(id),
+	`name` VARCHAR(256) NOT NULL
+);
