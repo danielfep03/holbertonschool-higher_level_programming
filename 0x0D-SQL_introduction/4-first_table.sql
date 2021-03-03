683415c874f2fde25baecda4cfa2bf88bb8317d3
Daniel Amado <danielfep.am@gmail.com>
2021-03-03 03:15:51 -0500

    Create a table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
--- script that creates a table called first_table in the current database
-
-CREATE TABLE IF NOT EXISTS first_table (
+-- Script that creates a table
+CREATE TABLE IF NOT EXISTS first_table(
 	`id` INTEGER UNSIGNED,
 	`name` VARCHAR(256)
-);
+)
