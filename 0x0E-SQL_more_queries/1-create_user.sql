ecfbea9a7cd62e7566b4fe0c866f73f71c29734c
Daniel Amado <danielfep.am@gmail.com>
2021-03-02 19:31:44 -0500

    Script thta create a new use

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Script thta create a new user
+-- Create a new user and grant all privileges
+CREATE USER IF NOT EXISTS'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
