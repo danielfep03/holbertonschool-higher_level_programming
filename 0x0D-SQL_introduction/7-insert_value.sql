fa1e6b16e057157ccb3debdc8d7e3e543be8d69e
Daniel Amado <danielfep.am@gmail.com>
2021-03-03 21:17:07 -0500

    Insert a new rpw in a table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
---  script that inserts a new row in the table first_table
-INSERT INTO first_table (id, name)VALUES(89, 'Holberton School');
+-- Insert a new row in a table
+INSERT INTO first_table (`id`, `name`) VALUES (89, `Holberton School`)
