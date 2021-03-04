52c527a58a146c8f5880925d522f250ad5b2045f
Daniel Amado <danielfep.am@gmail.com>
2021-03-03 21:01:12 -0500

    Prints a table description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
--- script that prints the full description of the table first_table from the database hbtn_0c_0
-
-SHOW CREATE TABLE first_table;
+-- script that prints the full description of a table
+SHOW TABLE first_table;
