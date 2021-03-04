0a0860fcabdda070725e2af3979fe3034dd9e69e
Daniel Amado <danielfep.am@gmail.com>
2021-03-04 01:06:22 -0500

    Script that list all the numbers of the record with the same score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Script that list all the numbers of the record with the same score
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score HAVING COUNT(*)>1 ORDER BY number DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score DESC;
