0e735fceaea06c96c9e33d4f67abb44d274488e6
Daniel Amado <danielfep.am@gmail.com>
2021-03-23 14:30:33 -0500

    Hot fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -9,11 +9,11 @@ class Square extends Rectangle {
 
   charPrint (c) {
     if (c === undefined) {
-      for (let i = 0; i <= this.size; i++) {
+      for (let i = 0; i < this.size; i++) {
         console.log(('X'.repeat(this.size)));
       }
     } else {
-      for (let i = 0; i <= this.size; i++) {
+      for (let i = 0; i < this.size; i++) {
         console.log((c.repeat(this.size)));
       }
     }
