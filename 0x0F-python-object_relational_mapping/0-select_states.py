9960a7069bcb763c43dddd942040cf1c0faddca8
Daniel Amado <danielfep.am@gmail.com>
2021-03-25 04:19:44 -0500

    script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' Script that lists all states from the database hbtn_0e_0_usa '''
+import MySQLdb
+from sys import argv
+
+
+def mysqlconnect():
+    ''' Script that lists all states from the database hbtn_0e_0_usa '''
+    db_connection = MySQLdb.connect('localhost', argv[1], argv[2],
+                                    argv[3], port=3306)
+    cursor = db_connection.cursor()
+    cursor.execute('SELECT * FROM states;')
+    for x in cursor:
+        print(x)
+    db_connection.close()
+
+
+mysqlconnect()
+
+if __name__ != '__main__':
+    exit
