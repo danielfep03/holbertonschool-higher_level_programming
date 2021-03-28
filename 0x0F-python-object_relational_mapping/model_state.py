da5258db3501d410a046413181f2dd16a1de62b1
Daniel Amado <danielfep.am@gmail.com>
2021-03-27 19:04:42 -0500

    Created State class

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+Class definition of a State
+'''
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('mysql://root:root@localhost:3306/hbtn_0e_6_usa')
+Base = declarative_base()
+
+
+class State(Base):
+    ''' State class definition for a State object in a database connection '''
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+
+Session = sessionmaker(engine)
+session = Session()
+
+Base.metadata.create_all(engine)
