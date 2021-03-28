43a81746cc8ffef5cb9e4d4057f346572fcc22ff
Daniel Amado <danielfep.am@gmail.com>
2021-03-27 19:13:06 -0500

    Hot Fix

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,12 +2,9 @@
 '''
 Class definition of a State
 '''
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql://root:root@localhost:3306/hbtn_0e_6_usa')
 Base = declarative_base()
 
 
@@ -17,9 +14,3 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True,
                 nullable=False)
     name = Column(String(128), nullable=False)
-
-
-Session = sessionmaker(engine)
-session = Session()
-
-Base.metadata.create_all(engine)
