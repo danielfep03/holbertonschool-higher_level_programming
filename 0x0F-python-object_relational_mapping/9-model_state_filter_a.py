d7af404a8b9499010dd4337e2f0745ebffdc75e2
Daniel Amado <danielfep.am@gmail.com>
2021-03-28 03:41:34 -0500

    script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-''' '''
+''' Script that lists States objects that contain the letter a from a database
+'''
 
 from model_state import Base, State
 from sqlalchemy import create_engine
