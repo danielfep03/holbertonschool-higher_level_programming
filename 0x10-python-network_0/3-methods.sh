9ccaee85e5d035f95d0516a578656d68654ad779
Daniel Amado <danielfep.am@gmail.com>
2021-04-08 23:56:30 -0500

    [UPDATED] Hot fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This is a super power
- curl -iX OPTIONS http://89656d599ee6.99e27b5c.hbtn-cod.io:5000 | grep -i "allow" | cut -d " " -f 2-
+ curl -sI -X OPTIONS "$1" | grep -i "allow" | cut -d " " -f 2-
