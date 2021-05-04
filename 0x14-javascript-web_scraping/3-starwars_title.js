c871c2ee8b4921f5fbecbec19981b00ce32922af
Daniel Amado <danielfep.am@gmail.com>
2021-05-04 14:38:23 -0500

    [UPDATED] Typo fixed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,5 +5,5 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request(url, (err, response, body) => {
   if (err) throw err;
-  console.error(JSON.parse(body).title);
+  console.log(JSON.parse(body).title);
 });
