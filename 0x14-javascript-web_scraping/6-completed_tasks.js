74b6a342e3797d7a2f614ab175b7944fcf18b8dd
Daniel Amado <danielfep.am@gmail.com>
2021-05-04 19:47:47 -0500

    [ADDED] script that computes the number of tasks completed by user id.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,19 +3,17 @@
 const request = require('request');
 const url = process.argv[2];
 const taskDone = {};
-let lastId = 1;
-let acum = 0;
 
-request(url, (err, response, body) => {
+request(url, 'utf-8', (err, response, body) => {
   if (err) throw err;
   const data = JSON.parse(body);
   data.forEach((element) => {
     if (element.completed === true) {
-      if (element.userId !== lastId) {
-        acum = 0;
-        lastId = element.userId;
+      if (taskDone[element.userId] === undefined) {
+        taskDone[element.userId] = 1;
+      } else {
+        taskDone[element.userId]++;
       }
-      taskDone[element.userId] = ++acum;
     }
   });
   console.log(taskDone);
